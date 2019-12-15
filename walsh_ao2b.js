ba2e5f23f76d65b308c8cb7da77649f63401af60
tjwalsh2 <58918013+tjwalsh2@users.noreply.github.com>
2019-12-15 14:41:16 -0600

    Adding Assignment 2

---
+++ walsh_ao2b.js
@@ -0,0 +1,21 @@
+/* Tyler Walsh
+walsh_ao2b.js
+19WI_INFO_2124_WW
+Thoendel
+12/15/2019 */
+
+let myName = "Tyler Walsh";
+var myAge = "20";
+const myHair = "Black";
+const myEye = "Blue";
+
+console.log();
+console.log("My Name:");
+console.log(myName);
+console.log("My Age:");
+console.log(myAge);
+console.log("My Hair Color:");
+console.log(myHair);
+console.log("My Eye Color");
+console.log(myEye);
+console.log();
